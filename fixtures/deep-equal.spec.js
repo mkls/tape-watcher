68584801b34123fd736485d713da364186a4afd2
mkls <mr.antal@gmail.com>
2017-01-25 20:41:50 +0100

    Some more cleanup

--- fixtures/deep-equal.spec.js
+++ fixtures/deep-equal.spec.js
@@ -1,6 +1,6 @@
 var test = require('tape')
 
-test('complex object', function(t) {
+test('very complex object', function(t) {
     t.deepEqual(
         {a: 2, b: 23, c: {t: 'asdf'}},
         {a: 2, b: 23, c: {t: {a: {a: 2, b: 23, c: {t: {a: {a: 2, b: 23, c: {t: {a: 54}}}}}}}}}
