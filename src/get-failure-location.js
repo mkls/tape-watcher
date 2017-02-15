e16c4178ba62ae921199a4975f4eccd6ad58189c
mkls <mr.antal@gmail.com>
2017-02-15 23:35:04 +0100

    Failure location from error stack

--- src/get-failure-location.js
+++ src/get-failure-location.js
@@ -1,3 +1,19 @@
-module.exports = function () {
+module.exports = function (dirname, stack) {
+    if (typeof stack !== 'string') {
+        return undefined
+    }
 
+    var nonNodeModuleLines = stack
+        .split('\n')
+        .map(row => row.match(/(?:\/|[a-zA-Z]:\\)[^:\s]+:(?:\d+)(?::\d+)?/))
+        .filter(location => location && location[0].indexOf('node_modules') === -1)
+
+    if (nonNodeModuleLines.length >= 1) {
+        var location = nonNodeModuleLines[0][0]
+
+        return location.indexOf(dirname) > -1 ?
+            location.slice(dirname.length + 1) :
+            location
+    }
+    return undefined
 }
